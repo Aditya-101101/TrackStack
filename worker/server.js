cba664e8e65c6101f70890a51def6583c687920d
Aditya Kumar <aditya8799singh.com>
2026-05-27 01:42:01 +0530

    fix to deploy worker as webservice on render

---
+++ worker/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "./src/index.js";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("TrackStack Worker Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Worker server running on port ${PORT}`
+  );
+});
