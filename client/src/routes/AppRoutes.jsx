ba022fa9949bb9b270ca266d0377178d320dff3d
Aditya Kumar <aditya8799singh@gmail.com>
2026-05-28 01:18:28 +0530

    route import fix

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "../pages/LandingPage.jsx";
+import LandingPage from "../pages/landingPage/LandingPage.jsx";
 import ProtectedRoute from "../components/ProtectedRoute.jsx";
 
 import Login from "../pages/auth/Login.jsx";
