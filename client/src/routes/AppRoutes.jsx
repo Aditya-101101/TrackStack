e788895382fbcbd1da4f54b5799396b9314e6ee2
Aditya Kumar <aditya8799singh@gmail.com>
2026-05-28 01:21:37 +0530

    routing fix

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "../pages/landingPage/LandingPage.jsx";
+
 import ProtectedRoute from "../components/ProtectedRoute.jsx";
 
 import Login from "../pages/auth/Login.jsx";
 import Register from "../pages/auth/Register.jsx";
 
 import DashboardLayout from "../layouts/DashboardLayout.jsx";
+
 import Dashboard from "../pages/dashboard/Dashboard.jsx";
 
 import Monitors from "../pages/monitors/Monitor.jsx";
@@ -14,35 +17,80 @@ import EditMonitor from "../pages/monitors/EditMonitor.jsx";
 import MonitorDetails from "../pages/monitors/MonitorDetails.jsx";
 
 import Incidents from "../pages/incidents/Incidents.jsx";
+
 import Settings from "../pages/settings/settings.jsx";
+
 import NotFound from "../pages/NotFound.jsx";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      
+      {/* PUBLIC ROUTES */}
       <Route
         path="/"
+        element={<LandingPage />}
+      />
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+      {/* PROTECTED ROUTES */}
+      <Route
         element={
           <ProtectedRoute>
             <DashboardLayout />
           </ProtectedRoute>
         }
       >
-        <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
 
-        <Route path="monitors" element={<Monitors />} />
-        <Route path="monitors/create" element={<CreateMonitor />} />
-        <Route path="monitors/:id/edit" element={<EditMonitor />} />
-        <Route path="monitors/:id" element={<MonitorDetails />} />
+        <Route
+          path="/monitors"
+          element={<Monitors />}
+        />
 
-        <Route path="incidents" element={<Incidents />} />
-        <Route path="settings" element={<Settings />} />
+        <Route
+          path="/monitors/create"
+          element={<CreateMonitor />}
+        />
+
+        <Route
+          path="/monitors/:id/edit"
+          element={<EditMonitor />}
+        />
+
+        <Route
+          path="/monitors/:id"
+          element={<MonitorDetails />}
+        />
+
+        <Route
+          path="/incidents"
+          element={<Incidents />}
+        />
+
+        <Route
+          path="/settings"
+          element={<Settings />}
+        />
       </Route>
-      <Route path="*" element={<NotFound />} />
+
+      {/* 404 */}
+      <Route
+        path="*"
+        element={<NotFound />}
+      />
     </Routes>
   );
 }
